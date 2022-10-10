e764b212bf16e9b4c4e760c5cb5f6068642bd648
panxiao81 <pan-xiao@live.cn>
2022-10-10 19:33:19 +0800

    edit lsp handlers for neovim v0.8

--- nvim/.config/nvim/lua/panxiao81/lsp/handlers.lua
+++ nvim/.config/nvim/lua/panxiao81/lsp/handlers.lua
@@ -46,7 +46,18 @@ end
 
 local function lsp_highlight_document(client)
   -- Set autocommands conditional on server_capabilities
-  if client.resolved_capabilities.document_highlight then
+    if client.server_capabilities.documentHighlightProvider then
+        vim.api.nvim_exec(
+            [[
+            augroup lsp_document_highlight
+                autocmd! * <buffer>
+                autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+                autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+            augroup END
+        ]],
+            false
+        )
+    elseif client.resolved_capabilities.document_highlight then
     vim.api.nvim_exec(
       [[
       augroup lsp_document_highlight
@@ -86,7 +97,12 @@ end
 
 M.on_attach = function(client, bufnr)
   if client.name == "tsserver" then
-    client.resolved_capabilities.document_formatting = false
+    if client.resolved_capabilities then
+        client.resolved_capabilities.document_formatting = false
+    end
+    if client.server_capabilities then
+        client.server_capabilities.documentFormattingProvider = false
+    end
   end
   lsp_keymaps(bufnr)
   lsp_highlight_document(client)
